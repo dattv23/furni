a91e1678a1a0b01d5ec3f4581b10a6ac3d59b7a4
tuantran33 <161140363+tuantran33@users.noreply.github.com>
2024-03-28 20:43:28 +0700

    feat: create ui home, contact, service, comfirm payment

---
+++ furni/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace furni.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Detail()
+        {
+            return View();
+        }
+        public IActionResult ShowProducts()
+        {
+            return View();
+        }
+
+        public IActionResult Payment()
+        {
+            return View();
+        }
+        public IActionResult Comfirm()
+        {
+            return View();
+        }
+
+        public IActionResult Thanks()
+        {
+            return View();
+        }
+    }
+}
