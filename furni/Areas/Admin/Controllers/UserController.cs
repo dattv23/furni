3fd3fa5304c32da1fddcaefe95a4a34f62554cc9
KhacPK2003 <phamkhac2003@gmail.com>
2024-04-06 23:09:31 +0700

    feat: Create,Update, Create, Index for Category, Orders, Product, User

---
+++ furni/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using furni.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace furni.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "admin")]
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            var user = _context.Users.ToList();
+            return View(user);
+        }
+        public IActionResult Delete()
+        {
+            var user = _context.Users.ToList();
+            return View(user);
+        }
+        public IActionResult Create()
+        {
+            var user = _context.Users.ToList();
+            return View(user);
+        }
+        public IActionResult Update()
+        {
+            var user = _context.Users.ToList();
+            return View(user);
+        }
+    }
+}
