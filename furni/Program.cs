1f7b7ef1603cd2d6081f69a542e0a15f2a61ab66
tiepiumay <nguyenngoctieptn@gmail.com>
2024-04-11 17:14:59 +0700

    refactor: ui login, ui  sign up

--- furni/Program.cs
+++ furni/Program.cs
@@ -5,8 +5,12 @@ using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using System.Globalization;
 using furni.Models.Services;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.AddSingleton<IVnPayService, VnPayService>();
@@ -18,6 +22,23 @@ builder.Services.AddSession(options =>
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
+
+
+// Thêm dịch vụ controllers và views
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddAuthentication().AddGoogle(options =>
+{
+    options.ClientId = "1022495641683-82pto6eboauvum4ua67am2frec33io2a.apps.googleusercontent.com";
+    options.ClientSecret = "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";
+})
+.AddFacebook(facebookOptions =>
+{
+    facebookOptions.AppId = "YOUR_FACEBOOK_APP_ID";
+    facebookOptions.AppSecret = "YOUR_FACEBOOK_APP_SECRET";
+});
+
 
 builder.Services.AddControllersWithViews()
     .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
@@ -68,10 +89,10 @@ app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapControllerRoute(
-      name: "areas",
-      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
+    //endpoints.MapControllerRoute(
+    //  name: "areas",
+    //  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    //);
     endpoints.MapControllerRoute(
       name: "default",
       pattern: "{controller=Home}/{action=Index}/{id?}"
