0a8b11754d8741c969a9184ff7b5067670048dec
KhacPK2003 <phamkhac2003@gmail.com>
2024-04-11 15:31:18 +0700

    add: settings for feat manage order

--- furni/Areas/Admin/Helpers/ApplicationMapper.cs
+++ furni/Areas/Admin/Helpers/ApplicationMapper.cs
@@ -11,6 +11,7 @@ namespace furni.Areas.Admin.Helpers
             CreateMap<ApplicationUser, UserModel>().ReverseMap();
             CreateMap<Product, ProductModel>().ReverseMap();
             CreateMap<Category, CategoryModel>().ReverseMap();
+            CreateMap<Order, OrdersModel>().ReverseMap();
         }
     }
 }
